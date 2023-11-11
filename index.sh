8f6429a097d38c816bd0bcfd202d5690f172880b
prohunter00017 <108502041+prohunter00017@users.noreply.github.com>
2023-11-11 13:00:07 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
 # Update package lists
+echo "Updating package lists..."
 sudo apt-get update
 
 # Install Apache2
+echo "Installing Apache2..."
 sudo apt-get install -y apache2
 
 # Install Git
+echo "Installing Git..."
 sudo apt-get install -y git
 
 # Enable Apache2 service
+echo "Enabling Apache2 service..."
 sudo systemctl enable apache2
 
 # Start Apache2 service
+echo "Starting Apache2 service..."
 sudo systemctl start apache2
 
-# Domain
-domain=$1
+# Prompt for domain
+echo "Please enter your domain:"
+read domain
 
-# SSH Git location
-git_location=$2
+# Prompt for SSH Git location
+echo "Please enter your SSH Git location:"
+read git_location
 
 # Clone the repository to the /var/www/$domain directory
+echo "Cloning the repository to the /var/www/$domain directory..."
 sudo git clone $git_location /var/www/$domain
 
-# GitHub username
-username=$3
+# Prompt for GitHub username
+echo "Please enter your GitHub username:"
+read username
 
-# GitHub repository name
-repo=$4
+# Prompt for GitHub repository name
+echo "Please enter your GitHub repository name:"
+read repo
 
-# GitHub personal access token
-token=$5
+# Prompt for GitHub personal access token
+echo "Please enter your GitHub personal access token:"
+read token
 
-# Webhook URL
-webhook_url=$6
+# Prompt for webhook URL
+echo "Please enter your webhook URL:"
+read webhook_url
 
 # Create the webhook
+echo "Creating the webhook..."
 curl -X POST -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$username/$repo/hooks -d '{
   "name": "web",
   "active": true,
@@ -48,19 +61,24 @@ curl -X POST -H "Authorization: token $token" -H "Accept: application/vnd.github
 }'
 
 # Install Certbot
+echo "Installing Certbot..."
 sudo apt-get install -y certbot python3-certbot-apache
 
 # Obtain and install SSL certificate
+echo "Obtaining and installing SSL certificate..."
 sudo certbot --apache -n --agree-tos --email zkounima1@protonmail.com -d $domain
 
 # Configure Apache to serve the website
+echo "Configuring Apache to serve the website..."
 echo "<VirtualHost *:80>
     ServerName $domain
     DocumentRoot /var/www/$domain
 </VirtualHost>" | sudo tee /etc/apache2/sites-available/$domain.conf
 
 # Enable the site
+echo "Enabling the site..."
 sudo a2ensite $domain.conf
 
 # Reload Apache to apply the changes
+echo "Reloading Apache to apply the changes..."
 sudo systemctl reload apache2
